deac54c421629da8d95d76b7fb245bb32cb163c8
Chizoba <chizyraky@gmail.com>
2020-04-25 18:15:51 +0300

    resized frame

--- BookStore.py
+++ BookStore.py
@@ -66,7 +66,7 @@ def view_all():
     cur = conn.cursor()
 
     # Insert into database
-    cur.execute("select * from books
+    cur.execute("select * from books"
                 )
     # Commit Changes
     conn.commit()
@@ -95,26 +95,22 @@ isbn_label.grid(row=1, column=2, padx=30)
 
 # Creating the execution buttons
 view_all_btn = Button(root, text="View all", font=('arial', 10, 'bold'), width=15)
-view_all_btn.grid(row=2, column=6)
-
+view_all_btn.grid(row=2, column=3)
 search_entry_btn = Button(root, text="Search entry", font=('arial', 10, 'bold'), width=15)
-search_entry_btn.grid(row=3, column=6)
-
+search_entry_btn.grid(row=3, column=3)
 add_entry_btn = Button(root, text="Add entry", font=('arial', 10, 'bold'), width=15, command=addEntry)
-add_entry_btn.grid(row=4, column=6)
-
+add_entry_btn.grid(row=4, column=3)
 update_selected_btn = Button(root, text="Update Selected", font=('arial', 10, 'bold'), width=15)
-update_selected_btn.grid(row=5, column=6)
-
+update_selected_btn.grid(row=5, column=3)
 delete_selected_btn = Button(root, text="Delete selected", font=('arial', 10, 'bold'), width=15)
-delete_selected_btn.grid(row=6, column=6)
+delete_selected_btn.grid(row=6, column=3)
+close_btn = Button(root, text="Close", font=('arial', 10, 'bold'), width=15)
+close_btn.grid(row=7, column=3)
 
 # Adding the display label
-data_entry_frame = Frame(root, bd=1, width=400, height=200, bg="cyan")
-data_entry_frame.grid(row=2, column=0)
+data_entry_frame = Frame(root, bd=1, width=400, height=250, bg="cyan")
+data_entry_frame.grid(row=2, rowspan=6,columnspan=3)
 
-close_btn = Button(root, text="Close", font=('arial', 10, 'bold'), width=15)
-close_btn.grid(row=7, column=6)
 # Commit Changes
 conn.commit()
 
