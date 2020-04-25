a3ec8cd6f7b92720982c0c4a6c608cfbf11fa70e
Chizoba <chizyraky@gmail.com>
2020-04-25 17:02:39 +0300

    added frame

--- BookStore.py
+++ BookStore.py
@@ -17,13 +17,13 @@ cur = conn.cursor()
 
 # Calling the execute() method to create table and perform SQL commands
 # Create table
-cur.execute("""create table books (
-            title text,
-            author text,
-            year integer,
-            isbn integer
-)
-""")
+# cur.execute("""create table books (
+#             title text,
+#             author text,
+#             year integer,
+#             isbn integer
+# )
+# """)
 
 # Create text boxes for the user entry
 book_title = Entry(root, width=20)
@@ -48,18 +48,18 @@ isbn_label.grid(row=1, column=2, padx=30)
 # Creating the execution buttons
 view_all_btn = Button(root, text="View all", font=('arial', 10, 'bold'), width=15)
 view_all_btn.grid(row=2, column=6)
-
 search_entry_btn = Button(root, text="Search entry", font=('arial', 10, 'bold'), width=15)
 search_entry_btn.grid(row=3, column=6)
-
 add_entry_btn = Button(root, text="Add entry", font=('arial', 10, 'bold'), width=15)
 add_entry_btn.grid(row=4, column=6)
-
 update_selected_btn = Button(root, text="Update Selected", font=('arial', 10, 'bold'), width=15)
 update_selected_btn.grid(row=5, column=6)
-
 delete_selected_btn = Button(root, text="Delete selected", font=('arial', 10, 'bold'), width=15)
 delete_selected_btn.grid(row=6, column=6)
+
+# Adding the display label
+data_entry_frame = LabelFrame(root, bd=1, width=400, height=200, bg="cyan")
+data_entry_frame.grid(row=2, column=0)
 
 close_btn = Button(root, text="Close", font=('arial', 10, 'bold'), width=15)
 close_btn.grid(row=7, column=6)
